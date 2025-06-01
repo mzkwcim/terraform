e7933ce4480a61caa3c923791af1f5acf386e6e4
Waldemar Krakowiak <wkrak98@gmail.com>
2025-06-01 18:44:59 +0200

    I've managed to fully configure domain controller with snap ins and users and group creation

--- copy_files.bat
+++ copy_files.bat
@@ -13,6 +13,18 @@ copy promote-dc.ps1 Z:\temp\promote-dc.ps1
 echo Kopiowanie pliku run_promotion.bat
 copy run_promotion.bat Z:\temp\run_promotion.bat
 
+echo Kopiowanie pliku konfiguracyjnego usług
+copy configure_services.ps1 Z:\temp\configure_services.ps1
+
+echo Kopiowanie pliku tworzacego użytkownikow
+copy create_users.ps1 Z:\temp\create_users.ps1
+
+echo Kopiowanie pliku tworzacego gpo
+copy create_gpo.ps1 Z:\temp\create_gpo.ps1
+
+echo Kopiowanie pliku tworzacego gpo
+copy create_shares.ps1 Z:\temp\create_shares.ps1
+
 echo Odmapowanie dysku
 net use Z: /delete
 
--- main.tf
+++ main.tf
@@ -43,3 +43,49 @@ resource "null_resource" "execute_dc_script" {
     interpreter = ["cmd", "/C"]
   }
 }
+
+resource "null_resource" "wait_for_dc_ready" {
+  depends_on = [null_resource.execute_dc_script]
+
+  provisioner "local-exec" {
+    command     = "echo Czekam 15 minut na zakończenie promocji DC... && ping -n 900 127.0.0.1 > nul"
+    interpreter = ["cmd", "/C"]
+  }
+}
+
+resource "null_resource" "configure_additional_services" {
+  depends_on = [null_resource.wait_for_dc_ready]
+
+  provisioner "local-exec" {
+    command     = "configure_services.bat"
+    interpreter = ["cmd", "/C"]
+  }
+}
+
+# Konfiguracja OU i użytkowników
+resource "null_resource" "create_users_and_ous" {
+  depends_on = [null_resource.configure_additional_services]
+
+  provisioner "local-exec" {
+    command     = "create_users.bat"
+    interpreter = ["cmd", "/C"]
+  }
+}
+
+resource "null_resource" "create_advanced_gpos" {
+  depends_on = [null_resource.create_users_and_ous]
+
+  provisioner "local-exec" {
+    command     = "create_advanced_gpo.bat"
+    interpreter = ["cmd", "/C"]
+  }
+}
+
+resource "null_resource" "create_shares" {
+  depends_on = [null_resource.create_users_and_ous, null_resource.create_advanced_gpos]
+
+  provisioner "local-exec" {
+    command     = "create_shares.bat"
+    interpreter = ["cmd", "/C"]
+  }
+}
